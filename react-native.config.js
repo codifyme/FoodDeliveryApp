715d36d9d2f4043c6dde65f637643c169e58a0e8
Angel Cochachez <acochachez@gmail.com>
2021-03-29 22:49:43 -0700

    Restaurant Screem: added order quantity, calories, items in cart

--- react-native.config.js
+++ react-native.config.js
@@ -3,5 +3,9 @@ module.exports={
         ios:{},
         android:{}
     },
-    assets:["./assets/fonts/"]
+    assets:["./assets/fonts/"],
+    commands: require('./path-to-commands.js'),
+    hooks: {
+        prelink: './path-to-a-prelink-hook',
+      },
 }
